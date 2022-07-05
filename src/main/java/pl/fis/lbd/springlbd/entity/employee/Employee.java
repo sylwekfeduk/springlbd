74bcbe5eb22fee10ea1154bbe2b5594f349f28be
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-05 16:17:21 +0200

    exercise 16

---
+++ src/main/java/pl/fis/lbd/springlbd/entity/employee/Employee.java
@@ -0,0 +1,11 @@
+package pl.fis.lbd.springlbd.entity.employee;
+
+public class Employee {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private long pesel;
+    private String idNumber;
+    private String phoneNumber;
+}
