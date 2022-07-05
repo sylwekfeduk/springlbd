72917b88161cd0c81f976636423eec6bdcb038b1
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-05 14:28:21 +0200

    exercise 10 @Primary

--- src/main/java/pl/fis/lbd/springlbd/service/employee/EmployeeImpl1.java
+++ src/main/java/pl/fis/lbd/springlbd/service/employee/EmployeeImpl1.java
@@ -1,10 +1,12 @@
 package pl.fis.lbd.springlbd.service.employee;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Primary
 public class EmployeeImpl1 implements EmployeeService{
     @Override
     public List<EmployeeService> findAll() {
