a91ece1573bb02d8df092bf75ac9cbed9c5670c5
sylwekfeduk <75621927+sylwekfeduk@users.noreply.github.com>
2022-07-05 14:18:50 +0200

    first commit

---
+++ src/main/java/pl/fis/lbd/springlbd/SpringlbdApplication.java
@@ -0,0 +1,13 @@
+package pl.fis.lbd.springlbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/fis/lbd/springlbd/SpringlbdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.fis.lbd.springlbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
